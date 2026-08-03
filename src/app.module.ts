d8f46656b95384943619f3b4e54f974feff297dc
nishonaliyevfarrux7-ui <nishonaliyevfarrux7@gmail.com>
2026-08-03 12:38:12 +0500

    categories,assistant

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './core/database/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { SeederModule } from './core/seed/seeder.module';
-<<<<<<< HEAD
+
 import { CategoriesModule } from './categories/categories.module';
 import { CourseAssistantModule } from './course-assistant/course-assistant.module';
-
-=======
 import { UserModule } from './users/users.module';
->>>>>>> b35bbea59f24d2ddb6ecd915585c026768af86ef
+
 
 @Module({
   imports: [
