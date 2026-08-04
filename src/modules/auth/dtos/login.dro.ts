4aede8d1b3fd45012a7afaf31ef112ce9c6a5b29
kebyu007 <kebyu001@gmail.com>
2026-08-04 11:19:32 +0500

    swagger for auth

--- src/modules/auth/dtos/login.dro.ts
+++ src/modules/auth/dtos/login.dro.ts
@@ -1,27 +1,20 @@
+import { ApiProperty } from "@nestjs/swagger";
 import {
   IsPhoneNumber,
   IsString,
-  IsStrongPassword,
 } from "class-validator";
 
 export class LoginDto {
+  @ApiProperty({
+    example: "+998901234567"
+  })
   @IsString()
   @IsPhoneNumber()
   phone!: string;
 
+  @ApiProperty({
+    example: "@Kebyu007"
+  })
   @IsString()
-  @IsStrongPassword(
-    {
-      minLength: 8,
-      minLowercase: 1,
-      minUppercase: 1,
-      minNumbers: 1,
-      minSymbols: 1,
-    },
-    {
-      message:
-        "Parol juda kuchsiz (katta va kichik harflar, raqam va belgi talab qilinadi).",
-    },
-  )
   password!: string;
 }
