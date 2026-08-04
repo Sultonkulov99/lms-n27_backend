dc47a69a78a6665d036299a870486e458a108da5
bekzod <bekzodbaratov454@gmail.com>
2026-08-04 10:00:46 +0500

    ..

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './core/database/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { SeederModule } from './core/seed/seeder.module';
+<<<<<<< HEAD
 import { CoursesModule } from './modules/courses/courses.module';
 
 import { CategoriesModule } from './modules/categories/categories.module';
 import { CourseAssistantModule } from './modules/course-assistant/course-assistant.module';
 import { UserModule } from './modules/users/users.module';
 
+=======
+import { MentorModule } from './config/modules/mentor.module';
+>>>>>>> ebf26f7 (..)
 
 @Module({
   imports: [
@@ -16,7 +20,11 @@ import { UserModule } from './modules/users/users.module';
     }),
     PrismaModule,
     SeederModule,
+<<<<<<< HEAD
     CoursesModule,
+=======
+    MentorModule
+>>>>>>> ebf26f7 (..)
   ],
 }) 
 export class AppModule {}
