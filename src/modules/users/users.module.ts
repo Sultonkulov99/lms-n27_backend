856c325c1f6f0daec88a8c284ac98ffd410e316f
dostonkomilov7 <doston.komilov7@icloud.com>
2026-08-04 12:18:34 +0500

    database

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,10 +1,9 @@
 import { Module } from "@nestjs/common";
 import { UserService } from "./users.service";
 import { UserController } from "./users.controller";
-import { PrismaService } from "src/core/database/prisma.service";
 
 @Module({
-    providers: [UserService, PrismaService],
+    providers: [UserService],
     controllers: [UserController],
 })
 
