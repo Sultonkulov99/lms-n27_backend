80899d108889f39eb652866bee3d43d366172c14
Azimxon <axewarred@gmail.com>
2026-08-04 10:22:20 +0500

    Fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,14 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './core/database/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { SeederModule } from './core/seed/seeder.module';
-<<<<<<< HEAD
 import { CoursesModule } from './modules/courses/courses.module';
 
 import { CategoriesModule } from './modules/categories/categories.module';
 import { CourseAssistantModule } from './modules/course-assistant/course-assistant.module';
 import { UserModule } from './modules/users/users.module';
-
-=======
 import { MentorModule } from './config/modules/mentor.module';
->>>>>>> ebf26f7 (..)
+import { AuthModule } from './modules';
+
 
 @Module({
   imports: [
@@ -20,11 +18,9 @@ import { MentorModule } from './config/modules/mentor.module';
     }),
     PrismaModule,
     SeederModule,
-<<<<<<< HEAD
     CoursesModule,
-=======
-    MentorModule
->>>>>>> ebf26f7 (..)
+    MentorModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
