6389f068a88c96c9517a2710724d7ba36f77d671
bekzod <bekzodbaratov454@gmail.com>
2026-08-05 11:34:37 +0500

    lessons

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { CourseAssistantModule } from "./modules/course-assistant/course-assista
 import { UserModule } from "./modules/users/users.module";
 import { AuthModule, SectionsModule } from "./modules";
 import { MentorModule } from "./modules/mentor/mentor.module";
+import { LessonsModule } from "./modules/lessons/lessons.module";
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { MentorModule } from "./modules/mentor/mentor.module";
     CourseAssistantModule,
     UserModule,
     MentorModule,
+    LessonsModule
   ],
 })
 export class AppModule {}
