d61e00fbb9c5b7c12fe149abce174923e90d688e
Xusanboy <xusannnazirboev@gmail.com>
2026-08-06 09:59:14 +0500

    feat:comments module added to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { AuthModule, SectionsModule } from "./modules";
 import { MentorModule } from "./modules/mentor/mentor.module";
 import { LessonsModule } from "./modules/lessons/lessons.module";
 import { UsersModule } from "./modules/users/users.module";
+import { CommentsModule } from "./modules/comments/comments.module";
 
 @Module({
   imports: [
@@ -24,7 +25,8 @@ import { UsersModule } from "./modules/users/users.module";
     CourseAssistantModule,
     UsersModule,
     MentorModule,
-    LessonsModule
+    LessonsModule,
+    CommentsModule
   ],
 })
 export class AppModule {}
