829cd3496a1dd2d6e4402e2aeaa9af4bd04445a6
bekzod <bekzodbaratov454@gmail.com>
2026-08-14 10:57:18 +0500

    Injectable qo'shdim

--- src/modules/students/students.service.ts
+++ src/modules/students/students.service.ts
@@ -1,10 +1,12 @@
-import { ConflictException, NotFoundException } from "@nestjs/common";
+import { ConflictException, Injectable, NotFoundException } from "@nestjs/common";
 import { UserRoles } from "@prisma/client";
 import { PrismaService } from "src/core/database/prisma.service";
 import { UpdateStudentDto } from "./dto/update-student.dto";
 
+@Injectable()
 export class StudentService {
     constructor(private readonly prisma: PrismaService) {}
+
 
     async getAllStudents() {
         const students = await this.prisma.user.findMany({
