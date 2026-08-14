69b01ed4a3291520fc935eaf3049ee476b161dc1
bekzod <bekzodbaratov454@gmail.com>
2026-08-14 11:04:53 +0500

    hu

--- src/modules/students/students.module.ts
+++ src/modules/students/students.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { StudentService } from "./students.service";
 import { StudentController } from "./students.controller";
+import { PrismaService } from "src/core/database/prisma.service";
 
 @Module({
-    providers: [StudentService],
+    providers: [StudentService, PrismaService],
     controllers: [StudentController],
 })
 export class StudentsModule {}
