e5cb211659ffbd177f523b7f1abfb0d90e15f22f
Azimxon <axewarred@gmail.com>
2026-08-20 11:55:27 +0500

    st patch

--- src/modules/students/students.service.ts
+++ src/modules/students/students.service.ts
@@ -1,26 +1,30 @@
-import { ConflictException, Injectable, NotFoundException } from "@nestjs/common";
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from "@nestjs/common";
 import { UserRoles } from "@prisma/client";
 import { PrismaService } from "src/core/database/prisma.service";
 import { UpdateStudentDto } from "./dto/update-student.dto";
 
 @Injectable()
 export class StudentService {
-    constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
-    async getAllStudents() {
-        const students = await this.prisma.user.findMany({
-            where: { role: UserRoles.STUDENT }, 
-            select: {
-                id: true,
-                file: true,
-                fullName: true,
-                phone: true,
-                role: true,
-                status: true,
-                created_at: true,
-                updated_at: true,
-            }
-        })
+  async getAllStudents() {
+    const students = await this.prisma.user.findMany({
+      where: { role: UserRoles.STUDENT },
+      select: {
+        id: true,
+        file: true,
+        fullName: true,
+        phone: true,
+        role: true,
+        status: true,
+        created_at: true,
+        updated_at: true,
+      },
+    });
 
     return {
       success: true,
@@ -30,12 +34,13 @@ export class StudentService {
 
   async getOneStudent(id: number) {
     const student = await this.prisma.user.findUnique({
-      where: { id, role: UserRoles.STUDENT },
+      where: { id },
       select: {
         id: true,
         fullName: true,
         phone: true,
         role: true,
+        status: true,
         created_at: true,
         updated_at: true,
       },
@@ -44,117 +49,101 @@ export class StudentService {
     if (!student) {
       throw new NotFoundException("Bunaqa student yo'q. Yana bilmadim:)!");
     }
- 
-    async getOneStudent(id: number) {
-        const student = await this.prisma.user.findUnique({
-            where: { id },
-            select: {
-                id: true,
-                fullName: true,
-                phone: true,
-                role: true,
-                status: true,
-                created_at: true,
-                updated_at: true,
-            },
-        });
+    return {
+      success: true,
+      data: student,
+    };
+  }
 
-        if (!student) {
-            throw new NotFoundException("Bunaqa student yo'q. Yana bilmadim:)!");
-        }
-        return {
-            success: true,
-            student,
-        }
+  async updateStudent(id: number, dto: UpdateStudentDto) {
+    const ExistingStudent = await this.prisma.user.findFirst({ where: { id } });
+
+    if (!ExistingStudent) {
+      throw new NotFoundException("Student is not found");
     }
 
-    async updateStudent(id: number, dto: UpdateStudentDto) {
-        const ExistingStudent = await this.prisma.user.findFirst({ where: { id }});
+    const student = await this.prisma.user.findFirst({
+      where: { role: UserRoles.ADMIN, phone: dto.phone },
+    });
 
-        if (!ExistingStudent) {
-            throw new NotFoundException("Student is not found");
-        }
+    if (student) throw new ConflictException("Student has already existed");
 
-        const student = await this.prisma.user.findFirst({ where: { role: UserRoles.ADMIN, phone: dto.phone } })
+    const updatedStudent = await this.prisma.user.update({
+      where: { id },
+      data: {
+        ...dto,
+      },
+    });
 
-        if (student) throw new ConflictException('Student has already existed')
+    return {
+      success: true,
+      data: updatedStudent,
+    };
+  }
 
-        const updatedStudent = await this.prisma.user.update({
-            where: { id },
-            data: {
-                ...dto,
-            }
-        })
+  async deleteStudent(id: number) {
+    const ExistingStudent = await this.prisma.user.findFirst({ where: { id } });
 
-        return {
-            success: true,
-            data: updatedStudent,
-        }
+    if (!ExistingStudent) throw new NotFoundException("Student is not found");
+
+    try {
+      await this.prisma.user.update({
+        where: { id },
+        data: { status: "INACTIVE" },
+      });
+
+      return {
+        success: true,
+        message: "Student successfully deleted (soft delete)",
+      };
+    } catch (error: any) {
+      console.error("Delete student error:", error);
+      throw new Error(`Failed to delete student: ${error.message}`);
     }
+  }
 
-    async deleteStudent(id: number) {
-        const ExistingStudent = await this.prisma.user.findFirst({ where: { id } })
-
-        if (!ExistingStudent) throw new NotFoundException("Student is not found")
-
-        try {
-            await this.prisma.user.update({ 
-                where: { id },
-                data: { status: 'INACTIVE' }
-            });
-
-            return {
-                success: true,
-                message: "Student successfully deleted (soft delete)"
-            }
-        } catch (error: any) {
-            console.error('Delete student error:', error);
-            throw new Error(`Failed to delete student: ${error.message}`)
-        }
-    }
-
-    async getMyCourses(userId: number) {
-        const payments = await this.prisma.payments.findMany({
-            where: {
-                userId,
-                status: true,
+  async getMyCourses(userId: number) {
+    const payments = await this.prisma.payments.findMany({
+      where: {
+        userId,
+        status: true,
+      },
+      include: {
+        course: {
+          select: {
+            id: true,
+            banner: true,
+            name: true,
+            description: true,
+            level: true,
+            price: true,
+            categories: {
+              select: { id: true, name: true },
             },
-            include: {
-                course: {
-                    select: {
-                        id: true,
-                        banner: true,
-                        name: true,
-                        description: true,
-                        level: true,
-                        price: true,
-                        categories: {
-                            select: { id: true, name: true },
-                        },
-                    },
-                },
-            },
-            orderBy: { created_at: "desc" },
-        });
+          },
+        },
+      },
+      orderBy: { created_at: "desc" },
+    });
 
-        const myCourses = payments.map((payment) => ({
-            course: {
-                id: payment.course.id,
-                title: payment.course.name,
-                description: payment.course.description,
-                thumbnail: payment.course.banner,
-                categoryId: payment.course.categories?.id,
-                category: {
-                    id: payment.course.categories?.id,
-                    name: payment.course.categories?.name,
-                },
-                createdAt: payment.created_at.toISOString(),
-                updatedAt: payment.created_at.toISOString(),
-            },
-            progress: 0,
-            lastAccessedAt: payment.created_at.toISOString(),
-        }));
+    const myCourses = payments.map((payment) => ({
+      course: {
+        id: payment.course.id,
+        title: payment.course.name,
+        description: payment.course.description,
+        thumbnail: payment.course.banner,
+        categoryId: payment.course.categories?.id,
+        category: {
+          id: payment.course.categories?.id,
+          name: payment.course.categories?.name,
+        },
+        createdAt: payment.created_at.toISOString(),
+        updatedAt: payment.created_at.toISOString(),
+      },
+      progress: 0,
+      lastAccessedAt: payment.created_at.toISOString(),
+    }));
 
-        return myCourses;
-    }
+    return myCourses;
+  }
 }
